ba0a476f089aeb156973bc6bfd237095db5df4f4
Khizar Hussain <khezarhussain1999@gmail.com>
2021-04-29 07:53:24 +0500

    added rudimentary cmd line args for image folder and annotation folder paths

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
+import cv2
+import os
+import sys
+import argparse
+
+
 def main():
-    print("well this works.")
+    parser = argparse.ArgumentParser(description="Simple MOT Annotator")
+    parser.add_argument("--images", type=str,
+                        help="path to images folder", default="./images")
+    parser.add_argument("--annots", type=str,
+                        help="path to annotations folder", default="./annotations")
+    args = parser.parse_args()
+
+    imgs_path = args.images
+    anot_path = args.annots
+
+    if not os.path.isdir(imgs_path):
+        sys.exit(f"[!] Error: path '{imgs_path}' for --images is invalid.")
+    if not os.path.isdir(anot_path):
+        sys.exit(f"[!] Error: path '{anot_path}' for --annots is invalid.")
+    print("It works till here.")
 
 
 if __name__ == "__main__":
